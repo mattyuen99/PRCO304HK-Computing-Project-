2446c0057df8792121bd2f1a15fd04f606c06ce7
mattyuen99 <65583254+mattyuen99@users.noreply.github.com>
2020-05-25 03:55:19 +0800

    Update footer.php
    Update footer with copyright about the project client: AccountDB

---
+++ includes/footer.php
@@ -0,0 +1,7 @@
+<footer class="container-fluid text-center bg-lightgray" style="background-color:#F8F8F8;">
+	<div class="copyrights" style="margin-top:25px;">
+		<p style="margin-top:-10px;">
+			Copyright © <?php echo COPYRIGHT_YEAR; ?> AccountDB, All Rights Reserved
+		</p>
+	</div>
+</footer>
