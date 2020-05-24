adf1cdad6b4751651128c942357d71cd5d43f683
mattyuen99 <65583254+mattyuen99@users.noreply.github.com>
2020-05-25 04:07:17 +0800

    Create user folder
    Contain navbar.php which showing the bar on top of the client front page
    Contain index.php which show the front page of the client with will fetch data from database

---
+++ user/includes/navbar.php
@@ -0,0 +1,19 @@
+<nav class="navbar navbar-default">
+    <div class="container">
+        <div class="navbar-header">
+            <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#navbar-collapse-1">
+                <span class="sr-only">Toggle navigation</span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+                <span class="icon-bar"></span>
+            </button>
+            <a class="navbar-brand" nohref>AccountDB - Manage Your Personal Information</a>
+        </div>
+
+        <div class="collapse navbar-collapse" id="navbar-collapse-1">
+            <ul class="nav navbar-nav navbar-right">
+                <li><a href="<?php echo $website_url ?>/logout.php">Logout</a></li>
+            </ul>
+        </div>
+    </div>
+</nav>
