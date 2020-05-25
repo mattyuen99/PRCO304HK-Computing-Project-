37b5652e9c6f788a8340f95d2b60b7ead7c6dcbf
mattyuen99 <65583254+mattyuen99@users.noreply.github.com>
2020-05-25 14:50:57 +0800

    Update index.php
    Added update function to index

--- user/index.php
+++ user/index.php
@@ -1,4 +1,4 @@
-<?php
+14:10 25/5/2020<?php
 
 include_once( dirname(__FILE__). '/../includes/config.php' );
 
@@ -18,7 +18,7 @@ include_once( $root_dir_path . '/user/includes/navbar.php' );
 	<h2 class="well custom_border_colored_line">
 		Viewing All Account
 		<?php
-		if($_SESSION['type'] == '2')
+        if($_SESSION['type'] == '2')
 		{
 		?>
 		<a href="#" data-toggle="modal" data-target="#Add_Modal" class="btn btn-primary pull-right">Add New</a>
@@ -66,10 +66,11 @@ th,td
 								<th class="no-sort">Password</th>
 								<th class="no-sort">Address</th>
 								<?php
-								if($_SESSION['type'] == '2')
+                                                                                                                                        	if($_SESSION['type'] == '2')
 								{
 								?>
 								<th class="no-sort">Delete</th>
+								<th class="no-sort">Update</th>
 								<?php
 								}
 								?>
@@ -117,10 +118,42 @@ th,td
 										<button type="submit" class="btn btn-xs btn-danger" name="delete_personnel" value="1" onclick="return confirm('Are you sure you want to delete this Personnel?')">Delete</button>
 									</form>
 								</td>
+								<td>
+									<a href="#" data-toggle="modal" data-target="#Update_Modal_<?php echo $serial_no; ?>" class="btn btn-xs btn-info">Update</a>
+								</td>
 								<?php
 								}
 								?>
 							</tr>
+
+<div class="modal fade" id="Update_Modal_<?php echo $serial_no; ?>" tabindex="-1" role="dialog" aria-labelledby="" aria-hidden="true">
+	<div class="modal-dialog">
+		<div class="modal-content">
+			<div class="modal-header">
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				<h3 class="modal-title" id="">Updating Account</h3>
+			</div>
+			<div class="modal-body">
+				<form method="POST" action="<?php echo $website_url . '/action.php'; ?>">
+					<input type="hidden" name="personnel_auto_id" value="<?php echo $personnel_auto_id; ?>" />
+					<label>Password:</label>
+					<div class="row">
+						<div class="form-group col-md-12 ">
+							<input type="text" class="form-control" name="password" value="" placeholder="Modify Password ....." required />
+						</div>
+					</div>
+										<div class="row">
+						<div class="form-group col-md-12">
+							<button style="margin-left:5px;" type="submit" class="btn btn-primary pull-right" name="update_personnel">Modify</button>
+						
+						</div>
+					</div>
+				</form>
+			</div>
+		</div>
+	</div>
+</div>
+
 		<?php
 			$serial_no++;
 			}
